57b0f6dc2989572fa1df6aa7821abed8a3d90950
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-20 13:21:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import { v4 as uuidv4 } from "uuid";
 import admin from "firebase-admin";
 import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 import sharp from "sharp";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
