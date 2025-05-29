973439901e8e94da77489ed5a10b12ec467d88f8
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-29 10:54:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,11 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
