a4dd7031de71cc522d60d48d915711ff4b97818d
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-30 10:56:42 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -471,12 +471,12 @@ function iniciarCheckPanelMessages() {
   console.log("▶️ Polling activado");
 }
 
+iniciarCheckPanelMessages();
+
 const estadoChat = localStorage.getItem('chatEstado');
 if (estadoChat !== 'cerrado') {
   restoreChat();
-}
-
-iniciarCheckPanelMessages(); else {
+} else {
   // ✅ Mostrar solo el mensaje de saludo guardado
   const saved = localStorage.getItem('chatMessages');
   if (saved) {
@@ -518,7 +518,6 @@ input.addEventListener('focus', () => {
     }
   }, 500);
 });
-
 input.addEventListener('blur', () => {
   blurActivo = true;
   setTimeout(() => {
