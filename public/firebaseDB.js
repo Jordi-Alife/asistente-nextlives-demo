e6127da01766712d6436a78a0f3a0cf87639d359
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-06-06 18:16:42 +0200

    Update firebaseDB.js

--- public/firebaseDB.js
+++ public/firebaseDB.js
@@ -1,27 +1,25 @@
-// firebaseDB.js (adaptado para <script> en HTML, no modules)
-importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore-compat.js");
+<!-- firebaseDB.js (modo clásico, sin imports ni workers) -->
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore-compat.js"></script>
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyB0vz-jtc7PRpdFfQUKvU9PevLEV8zYzO4",
+    authDomain: "nextlives-panel-soporte.firebaseapp.com",
+    projectId: "nextlives-panel-soporte",
+    storageBucket: "nextlives-panel-soporte.appspot.com",
+    messagingSenderId: "52725281576",
+    appId: "1:52725281576:web:4402c0507962074345161d"
+  };
 
-// Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyB0vz-jtc7PRpdFfQUKvU9PevLEV8zYzO4",
-  authDomain: "nextlives-panel-soporte.firebaseapp.com",
-  projectId: "nextlives-panel-soporte",
-  storageBucket: "nextlives-panel-soporte.appspot.com",
-  messagingSenderId: "52725281576",
-  appId: "1:52725281576:web:4402c0507962074345161d"
-};
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-// Exponer funciones necesarias en window.firestore
-window.firestore = {
-  db,
-  collection: firebase.firestore().collection,
-  doc: firebase.firestore().doc,
-  query: firebase.firestore().query,
-  where: firebase.firestore().where,
-  onSnapshot: firebase.firestore().onSnapshot
-};
+  window.firestore = {
+    db: db,
+    collection: (...args) => db.collection(...args),
+    doc: (...args) => db.doc(...args),
+    query: (...args) => args[0], // En compat, no se necesita query wrapper
+    where: (...args) => firebase.firestore.FieldPath.documentId().where(...args),
+    onSnapshot: (...args) => firebase.firestore().onSnapshot(...args)
+  };
+</script>
