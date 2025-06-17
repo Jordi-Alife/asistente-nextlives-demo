62f6a10a44c8bfb300527157138919c161a71935
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-06-17 15:58:20 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -747,6 +747,7 @@ function initializeChat(userUuid, lineUuid, language = 'en') {
       language: language,
       initialized: true
     };
+    cargarNombreFuneraria(userUuid);
 
     // ✅ Mostrar el ID de usuario en la interfaz
     const userInfoElement = document.getElementById('userIdDisplay');
