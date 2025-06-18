3eae185f6efd6b6fc36cc27e96398cf7dce66af1
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-06-18 13:53:21 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -598,7 +598,7 @@ if (messagesDiv.children.length === 0) {
   const esperarNombre = () => {
     const nombreDisponible = !!window.chatSystem?.nombre;
 
-    if (nombreDisponible || intentos >= 10) {
+    if (nombreDisponible || intentos >= 14) {
       const datosContexto = {
         nombre: window.chatSystem?.nombre || null,
         userUuid: window.chatSystem?.currentUser || null,
